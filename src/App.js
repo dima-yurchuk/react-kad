505254f8e7459794bb7e6c80b6d91fe7215fbad3
dima-yurchuk <dimayurchuk20@gmail.com>
2022-02-23 15:51:51 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from './components/Header/Header'
 import Input from "./components/Input/Input";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route/>
+            </Routes>
+        </BrowserRouter>
         <Header/>
         <Input title={'Title'} placeholder={'placeholder'}/>
     </div>
