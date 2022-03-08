108a879022b1ee3d44cdd59aae3cd1303f20eed0
Andrii Korniienko <79922681+akorniienko@users.noreply.github.com>
2022-03-08 19:56:07 +0200

    Update Header.jsx
    думаю так краще буде, більш деклеративний код і одразу все видно, не треба очима бігати і дивитися на змінну а потім до неї умову.
    також класна штука для подібних повторювань елементів робити масив і по ньому читати данні, приклад з картинками, а масив виносити в наприклад створену папку constants а там уже можеш фали стоворити відповідно до компоненти чи цілей цих констант

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,32 +3,47 @@ import './Header.scss'
 import Button from "../Button/Button";
 import {Link} from "react-router-dom";
 
+const HEADER_IMAGES = [
+    {
+        name: 'email',
+        alt: ''
+    },
+    {
+        name: 'whatsapp',
+        alt: ''
+    },
+    {
+        name: 'telegram',
+        alt: ''
+    },
+    {
+        name: 'youtube',
+        alt: ''
+    },
+];
+
 const Header = () => {
-    let auth = null;
-    if (window.location.pathname==='/'){
-        auth =  <div className={'header__auth'}>
-            <Link to={'/login'}>
-                <button className={'header__login'}>
-                    Вход
-                </button>
-            </Link>
-            <Link to={'/register'}>
-                <Button width={143} text={'Регистрация'}/>
-            </Link>
-        </div>;
-    }
     return (
         <div className={'header'}>
             <div className={'header__logo'}>
                 KAD
             </div>
             <div className={'header__link'}>
-                <img src={'/images/header/email.png'}/>
-                <img src={'/images/header/whatsapp.png'}/>
-                <img src={'/images/header/telegram.png'}/>
-                <img src={'/images/header/youtube.png'}/>
+                {HEADER_IMAGES.map(({name, alt}) => <img src={`/images/header/${name}.png`} alt={alt}/>)}
             </div>
-            {auth}
+            {
+                window.location.pathname==='/' && 
+                <div className={'header__auth'}>
+                    <Link to={'/login'}>
+                        <button className={'header__login'}>
+                            Вход
+                        </button>
+                    </Link>
+                    <Link to={'/register'}>
+                        <Button width={143} text={'Регистрация'}/>
+                    </Link>
+                </div>
+            }
         </div>
     );
 };
